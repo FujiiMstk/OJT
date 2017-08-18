749780fecf01450e12a0f45594ec5e026510719e
FujiiMstk <fujii_mstk@actis.co.jp>
2017-08-18 11:49:13 +0900

    modified:   CL.py 	modified:   SV.py

--- CL.py
+++ CL.py
@@ -1,82 +1,55 @@
 from __future__ import print_function
-
-import glob
+import time
 import sys
 import os
 import datetime
 import re
 
-#import socket
-from contextlib import closing
 
-"""     socket通信※使用しない
-class SVconnect:
-    def SVsend(self):
-        host = '127.0.0.1'
-        port = 4000
-        bufsize = 4096
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        with closing(sock):
-            sock.connect((host, port))
-            while True:
-                line = USERinput()
-                if len(line) == 0:
-                    break
-                for sline in line:
-                    sock.send(sline.encode('utf-8'))
-                    print("SV respons: %s " % sock.recv(bufsize).decode('utf-8'))
-        return
-"""
 class Checker:
+
     def EIforderCheck():
+
         if os.path.exists("../Fpool") == False:
             os.mkdir("../Fpool")
 
+
+
     def InputRule(Fnum, ChVal):
-
         index = 0
-
         if Fnum == 0:
             searchFormat = re.compile(r'[0-9]')
             Vlen = len(ChVal)
             if Vlen > 4:
                 print("""入力文字数が４桁より多いです。会員IDは４桁です。\n先ほどの入力文字数：%s文字""" % Vlen)
-
             elif Vlen < 4:
                 print("""入力文字数が４桁より少ないです。会員IDは４桁です。\n先ほどの入力文字数：%s文字""" % Vlen)
-
-
             for IDchar in ChVal:
-                index+=1
+                index += 1
                 SResult = searchFormat.search(IDchar)
                 if SResult == None:
                     print('%d文字目に使用不可文字「%s」が入力されています。' % (index, IDchar))
-                        
             return False
-
         elif Fnum == 1:
-            searchFormatHK = re.compile(r'[\u3040-\u30FF]') #ひらがな＆カタカナ
-            searchFormatKN = re.compile(r'[\u4E00-\u9FFF]') #漢字
-
+            searchFormatHK = re.compile(r'[\u3040-\u30FF]')  # ひらがな＆カタカナ
+            searchFormatKN = re.compile(r'[\u4E00-\u9FFF]')  # 漢字
             spaceIndex = 0
-
             Vlen = len(ChVal)
             if Vlen != 30:
                 if Vlen > 30:
                     print("入力文字数が多すぎます。お名前が入りきらない場合は管理者へ問い合わせてください。")
-
             for NameChar in ChVal:
                 index += 1
                 SResultHK = searchFormatHK.search(NameChar)
                 SResultKN = searchFormatKN.search(NameChar)
-                if index == 1 | index == Vlen :
+                if index == 1 | index == Vlen:
                     if spaceIndex > 0:
                         print('%dつ目のスペースが入力されています。')
                         spaceIndex += 1
                         continue
                     elif NameChar == '　':
-                        spaceIndex+=1
+                        spaceIndex += 1
                         continue
                     elif NameChar == ' ':
                         spaceIndex += 1
@@ -86,154 +59,190 @@ class Checker:
                     continue
                 elif SResultKN == None:
                     print('%d文字目に使用不可文字「%s」が入力されています。' % (index, NameChar))
-
             return False
-
         else:
             return True
 
 
 
+class IOcontrol:  # ファイル入出力
 
-class IOcontrol:    #ファイル入出力
+    # Fname = ""
 
-    #Fname = ""
+    def DExpt(func, writeV):  # ファイル出力
 
-    def DExpt(func, writeV):    #ファイル出力
         Checker.EIforderCheck()
 
-        Gdate = datetime.datetime.now()  #今の日時
-        Pdate = Gdate.strftime('%Y%m%d_%H%M%S') #日時のSTR化
+        Gdate = datetime.datetime.now()  # 今の日時
+        Pdate = Gdate.strftime('%Y%m%d_%H%M%S')  # 日時のSTR化
 
-        Fname = 'Expt_' + Pdate     #出力ファイル名「Expt_YYYYmmdd_HHMMSS」
+        Fname = 'Expt_' + Pdate  # 出力ファイル名「Expt_YYYYmmdd_HHMMSS」
 
-        #出力ファイル生成
-        f = open('../Fpool/%s.prf'% Fname, 'w')
-        f.write("%s %s" % (func , writeV))
+        # 出力ファイル生成
+        f = open('../Fpool/%s.prf' % Fname, 'w')
+        f.write("%s %s" % (func, writeV))
         f.close()
 
         return Pdate
 
-    def DInpt():     #ファイル読込み
+
+    def DInpt():  # ファイル読込み
         Checker.EIforderCheck()
+        Gret = 'brank'
 
         while True:
-
+            time.sleep(2)
             FName = IOcontrol.DSearch()
-
             if FName == False:
                 continue
 
-            #SV生成ファイルの読み込み
-            f = open('%s'% FName, 'r')
-            print('test')
-            Gret = f   #SVからの返事を格納
-            f.close()
+            # SV生成ファイルの読み込み
+            for gFName in FName:
 
-            #使用済みファイルのリネーム
-            rFName = 'cmp-' + FName
-            os.rename('../Fpool/%s'% FName , '../Fpool/%s'% rFName)
+                if gFName[0] == 'R':
+                    f = open('../Fpool/%s' % gFName, 'r')
+                    Gret = f.read()  # SVからの返事を格納
+                    f.close()
+                    # 使用済みファイルのリネーム
+                    rFName = 'cmp-' + gFName
+                    os.rename('../Fpool/%s' % gFName, '../Fpool/%s' % rFName)
 
-            return Gret
+                else:
+                    continue
+
+                return Gret
+
+
 
     def DSearch():
+
         Checker.EIforderCheck()
 
-        SearchFile = glob.glob('../Fpool/Expt_*.prf')
+        SearchFile = os.listdir('../Fpool/')
 
         rslLen = len(SearchFile)
+
         if rslLen == 0:
             return False
+
         else:
-            return True
-
-
-
+            return SearchFile
 
 
 
 class CLfunction:
+
     def NewCreate():
+
         while True:
+
             mainTop.CreateTop()
 
-            UserID = USERinput.inputID(1)
+            UserID = USERinput.inputID(2)
             UserData = USERinput.createUSER()
 
             UpData = UserID + ' '
+
             for instr in UserData:
                 UpData = UpData + instr
                 UpData = UpData + '/'
 
-            IOcontrol.DExpt('1',UpData )
+            IOcontrol.DExpt('1', UpData)
             result = IOcontrol.DInpt()
-            #result = 'OK'
+            # result = 'OK'
 
-            if result == 'OK':
+            if result == 'True':
                 print('\n登録が完了しました\n')
+
             else:
                 print('\n登録に失敗しました\n')
 
             FanCnt = USERinput.FunctionContinue()
+
             os.system('clear')
+
             if FanCnt == True:
                 continue
+
             elif FanCnt == False:
                 break
 
+
+
     def SearchUser():
+
         while True:
+
             mainTop.SearchTop()
 
             UserID = USERinput.inputID(2)
 
-            IOcontrol.DExpt('2',UserID )
+            IOcontrol.DExpt('2', UserID)
             result = IOcontrol.DInpt()
 
-            if UserID == result:
-                print('\n指定されたユーザ(UserID:%s)は存在します。\n'%UserID)
-            else :
+            if result == 'True':
+                print('\n指定されたユーザ(UserID:%s)は存在します。\n' % UserID)
+
+            else:
                 print('\n指定されたユーザ(UserID:%s)は存在しません。\n' % UserID)
 
+
             FanCnt = USERinput.FunctionContinue()
+
             os.system('clear')
+
             if FanCnt == True:
                 continue
+
             elif FanCnt == False:
                 break
 
+
+
     def DeleteUser():
+
         while True:
+
             mainTop.DeleteTop()
 
             UserID = USERinput.inputID(3)
 
             print('\n\n？？？を本当に削除しますか？')
+
             while True:
+
                 answer = input(' Y / N >>')
-                if answer == 'Y':
-                    IOcontrol.DExpt('3',UserID )
+
+                if answer == 'Y' or answer == 'y' or answer == 'yes' or answer == 'YES':
+                    IOcontrol.DExpt('3', UserID)
                     result = IOcontrol.DInpt()
-                    #result = 1
+                    # result = 1
 
                     if result == 1:
                         print("\n削除が完了しました。\n")
+
                     else:
                         print("\n削除が実行できませんでした。\n")
 
                     break
-                elif answer == 'N':
+
+                elif answer == 'N' or answer == 'n' or answer == 'no' or answer == 'NO':
                     print("\n\nユーザの削除を中止します")
                     break
+
                 else:
                     print("\n'Y'または'N'を入力してください\n")
                     continue
 
 
+
             FanCnt = USERinput.FunctionContinue()
+
             os.system('clear')
+
             if FanCnt == True:
                 continue
+
             elif FanCnt == False:
                 break
 
@@ -241,77 +250,92 @@ class CLfunction:
 
 
 
+# ユーザ入力
 
-
-
-
-
-#ユーザ入力
 class USERinput:
+
     def inputID(func):
+
         while True:
             userID = input("ユーザIDを入力してください>>")
 
-            #Chres = Checker.InputRule(0, userID)
-            #if Chres == False:
+            # Chres = Checker.InputRule(0, userID)
+            # if Chres == False:
             #    continue
-            print('ID-G')
 
             ##SVconnect#引数で照会/削除指定の関数作成
             IOcontrol.DExpt(2, userID)
-            print('SV-Expt')
+
             reqResult = IOcontrol.DInpt()
-            #reqResult = input('Enter or Other >>')
+
             if len(reqResult) == 0:
                 if func == 1:
                     print('')
-                    #return userID
+                    # return userID
+
                 elif func == 2:
                     print('\n\n指定されたユーザは存在しません。\n他のIDを指定してください')
                     continue
+
                 elif func == 3:
                     print('\n\n指定されたユーザは存在しません。\n他のIDを指定してください')
                     continue
+
+
 
             else:
                 if func == 1:
                     print('\n\n新規：ユーザデータが存在します。\n他のIDを指定してください')
                     continue
+
                 elif func == 2:
                     print('')
+
                 elif func == 3:
                     print('')
 
             print('\n')
+
             return userID
 
+
+
     def createUSER():
+
         userNAME = input("\nユーザ氏名を入力してください>>")
         userPLACE = input("\n住所を入力してください>>")
         userTEL = input("\nユーザ連絡先[TEL]を入力してください>>")
         userMAIL = input("\nユーザ連絡先[MAIL]を入力してください>>")
 
-        return  (userNAME, userPLACE, userTEL, userMAIL)
+        return (userNAME, userPLACE, userTEL, userMAIL)
+
+
 
     def FunctionContinue():
+
         while True:
             answer = input("\n\nこの機能を続けますか(Y/N)>>")
 
-            if answer == 'Y':
+            if answer == 'Y' or answer == 'y' or answer == 'yes' or answer == 'YES':
                 return True
-            elif answer == 'N':
+
+            elif answer == 'N' or answer == 'n' or answer == 'no' or answer == 'NO':
                 return False
+
             else:
                 print('\n入力できるのは Y または N のみです。')
                 continue
 
 
 
-#画面表示
+# 画面表示
+
 class mainTop:
+
     def printTop():
+
         print("""
-            
+        
             ##############################
             # メニューを選択してください #
             #                            #
@@ -321,21 +345,24 @@ class mainTop:
             #                            #
             # 0:終了                     #
             ##############################
-            
-            
+
             """)
+
         sl_Menu = input('>> ')
 
         return sl_Menu
 
+
+
     def printFin():
+
         os.system('clear')
 
         print("""
-            
+
             アプリを終了します。
             エンターキーを押してください。
-            
+
             """)
 
         end_Cnt = input('')
@@ -344,11 +371,14 @@ class mainTop:
             os.system('clear')
             sys.exit()
 
+
+
     def CreateTop():
+
         print("""
             ユーザの新規登録を行います。
             以下の入力要領に従って入力してください。
-            
+
             ################################################################
             # 入力内容                                                     #
             #                                                              #
@@ -363,11 +393,14 @@ class mainTop:
             ################################################################
             """)
 
+
+
     def SearchTop():
+
         print("""
             ユーザの検索を行います。
             以下の入力要領に従って入力してください。
-            
+
             ################################################################
             # 入力内容                                                     #
             #                                                              #
@@ -376,7 +409,10 @@ class mainTop:
             ################################################################
             """)
 
+
+
     def DeleteTop():
+
         print("""
             ユーザの削除を行います。
             以下の入力要領に従って入力してください。
@@ -390,50 +426,59 @@ class mainTop:
             """)
 
 
+
+
+
 if __name__ == '__main__':
 
     os.system('clear')
 
     while True:
-
         M_val = mainTop.printTop()
 
         print(M_val)
 
-        #新規登録
+        # 新規登録
         if M_val == '1':
             try:
                 os.system('clear')
                 CLfunction.NewCreate()
                 continue
+
             except:
                 os.system('clear')
                 print("1 の入力を受け付けました")
-        #参照
+
+        # 参照
         elif M_val == '2':
             try:
                 os.system('clear')
                 CLfunction.SearchUser()
                 continue
+
             except:
                 os.system('clear')
                 print("2 の入力を受け付けました")
-        #削除
+
+        # 削除
+
         elif M_val == '3':
             try:
                 os.system('clear')
                 CLfunction.DeleteUser()
                 continue
+
             except:
                 os.system('clear')
                 print("3 の入力を受け付けました")
-        #終了
+
+        # 終了
         elif M_val == '0':
             os.system('clear')
             mainTop.printFin()
-        #入力エラー
+
+        # 入力エラー
         else:
             os.system('clear')
             print("指定と異なる値です。もう一度入れなおしてください。")
             continue
-
--- SV.py
+++ SV.py
@@ -1,129 +1,121 @@
 from __future__ import print_function
-import sys
 import os
-import datetime
-import re
-
-import glob
+import time
 import redis
-
-#import socket
-#import select
-from contextlib import closing
-
-"""
-def main():
-  host = '127.0.0.1'
-  port = 4000
-  backlog = 10
-  bufsize = 4096
-
-  server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  readfds = set([server_sock])
-  try:
-    server_sock.bind((host, port))
-    server_sock.listen(backlog)
-
-    while True:
-      rready, wready, xready = select.select(readfds, [], [])
-      for sock in rready:
-        if sock is server_sock:
-          conn, address = server_sock.accept()
-          readfds.add(conn)
-        else:
-          msg = sock.recv(bufsize)
-          if len(msg) == 0:
-            sock.close()
-            readfds.remove(sock)
-          else:
-            print(msg.decode('utf-8'))
-            sock.send(msg)
-  finally:
-    for sock in readfds:
-      sock.close()
-  return
-"""
 
 class Checker:
     def EIforderCheck():
         if os.path.exists("../Fpool") == False:
             os.mkdir("../Fpool")
 
-class IOcontrol:    #ファイル入出力
-    def DExpt(FName, writeV):    #ファイル出力
-        Checker.EIforderCheck()
-        print('test')
-        #出力ファイル生成
-        if FName[0] == '-':
-            f = open('../Fpool/RSL%s'% FName, 'w')
-            f.write("%s" % writeV)
-            f.close()
 
 
-    def DInpt(FName):     #ファイル読込み
+class IOcontrol:  # ファイル入出力
+
+    def DExpt(FName, writeV):  # ファイル出力
+
         Checker.EIforderCheck()
+
+        # 出力ファイル生成
+        for picFName in FName:
+
+            if picFName[0] == 'E':
+                f = open('../Fpool/RSL-%s' % picFName, 'w')
+                f.write("%s" % writeV)
+                f.close()
+            elif picFName[0] == '-':
+                os.remove('../Fpool/%s' % picFName)
+
+
+
+    def DInpt(FName):  # ファイル読込み
+
+        Checker.EIforderCheck()
+
         Gret = None
 
-        #SV生成ファイルの読み込み
+        # SV生成ファイルの読み込み
         if FName[0] == 'E':
-            f = open('../Fpool/%s'% FName, 'r')
+            f = open('../Fpool/%s' % FName, 'r')
             for Inpt in f:
-                Gret =Inpt#f#.strip()    #SVからの返事を格納
+                Gret = Inpt  # f#.strip()    #SVからの返事を格納
+
             f.close()
 
-        #使用済みファイルのリネーム
-        if FName[0] != '-':
-            rFName = '-' + FName
-            os.rename('../Fpool/%s'% FName , '../Fpool/%s'% rFName)
-        #elif FName[0] == '-':
-        #    os.remove('../Fpool/%s' % FName)
+            os.rename('../Fpool/%s' % FName, '../Fpool/-%s' % FName)
 
         return Gret
+
+
 
     def DSearch():
         Checker.EIforderCheck()
 
         SearchFile = os.listdir('../Fpool/')
-
         rslLen = len(SearchFile)
+
         if rslLen == 0:
             return False
+
         else:
-            return SearchFile[0]
+            return SearchFile  # [0]
+
 
 def main():
     r = redis.Redis(host='localhost', port=6379, db=15)
 
+    TgFName = 'brank'
+    GetCLreq = 'brank'
+
     while True:
+        time.sleep(1)
         FName = IOcontrol.DSearch()
+
         if FName == False:
             continue
+
         else:
-            GetCLreq = IOcontrol.DInpt(FName)
+            for gFName in FName:
+                GetCLreq = IOcontrol.DInpt(gFName)
+                print(GetCLreq)
+
+                if GetCLreq != None:
+                    TgFName = gFName
+                    break
 
         if GetCLreq != None:
-            CLreq = GetCLreq.split()
 
+            CLreq = GetCLreq.split()
             if CLreq[0] == '1':
-                result = '1'#r.set(CLreq[1], CLreq[2])
+
+                result = r.set(CLreq[1], CLreq[2])
 
                 IOcontrol.DExpt(FName, result)
+
                 continue
+
+
 
             elif CLreq[0] == '2':
-                print('test')
-                result = '2'#r.exists(CLreq[1])
+
+                result = r.exists(CLreq[1])
+                print(result)
 
                 IOcontrol.DExpt(FName, result)
                 continue
+
+
 
             elif CLreq[0] == '3':
-                result = '3'#r.delete(CLreq[1])
+
+                result = r.delete(CLreq[1])
+                print(result)
 
                 IOcontrol.DExpt(FName, result)
                 continue
+
 
 
 if __name__ == '__main__':
-    main()
 
+    main()
